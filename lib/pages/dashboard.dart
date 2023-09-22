0cc679b7e8c3a214887f2a6ee031f00f3426395e
Gabriel Schorsch <gabrielschorschgsa@gmail.com>
2023-09-22 10:15:39 -0300

    feat: changed animation duration + added list to appbar

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -97,141 +97,133 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     _controller.animateTo(selectedMonth * 80.0 - 160,
-        duration: const Duration(milliseconds: 50), curve: Curves.easeIn);
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
 
     return Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.filter_list_rounded,
-                size: 32,
-              ),
-            ),
-          ],
+          toolbarHeight: 75,
+          leading: _getMonthsList(),
+          leadingWidth: MediaQuery.of(context).size.width,
         ),
-        body: CustomScrollView(slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate([
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .8,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _getMonthsList(),
-                    Center(
-                      child: SizedBox(
-                        height: 200,
-                        width: 200,
-                        child: PieChart(
-                          PieChartData(
-                            sections: mockupCategories
-                                .map((e) => PieChartSectionData(
-                                      color: Color(int.parse(
-                                          "0xFF${e.color!.replaceAll('#', '')}")),
-                                      showTitle: false,
-                                      value: mockupData
-                                          .where((element) =>
-                                              element.category?.id == e.id)
-                                          .map((e) => e.value)
-                                          .reduce((value, element) =>
-                                              value! + element!),
-                                    ))
-                                .toList(),
-                            pieTouchData: PieTouchData(
-                              enabled: true,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomScrollView(slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate([
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .8,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: SizedBox(
+                          height: 200,
+                          width: 200,
+                          child: PieChart(
+                            PieChartData(
+                              sections: mockupCategories
+                                  .map((e) => PieChartSectionData(
+                                        color: Color(int.parse(
+                                            "0xFF${e.color!.replaceAll('#', '')}")),
+                                        showTitle: false,
+                                        value: mockupData
+                                            .where((element) =>
+                                                element.category?.id == e.id)
+                                            .map((e) => e.value)
+                                            .reduce((value, element) =>
+                                                value! + element!),
+                                      ))
+                                  .toList(),
+                              pieTouchData: PieTouchData(
+                                enabled: true,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * .8,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.onPrimary,
+                Container(
+                  width: MediaQuery.of(context).size.width * .8,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 15,
+                          top: 20,
+                        ),
+                        child: Text(
+                          "Histórico",
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 15,
+                          top: 20,
+                        ),
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: mockupData.length,
+                            itemBuilder: (_, index) => ExpenseCard(
+                                  color: Color(int.parse(
+                                      "0xFF${mockupData[index].category!.color!.replaceAll('#', '')}")),
+                                  name: mockupData[index].name!,
+                                  value: mockupData[index].value!.toString(),
+                                )),
+                      )
+                    ],
+                  ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 15,
-                        top: 20,
-                      ),
-                      child: Text(
-                        "Histórico",
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 15,
-                        top: 20,
-                      ),
-                      child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: mockupData.length,
-                          itemBuilder: (_, index) => ExpenseCard(
-                                color: Color(int.parse(
-                                    "0xFF${mockupData[index].category!.color!.replaceAll('#', '')}")),
-                                name: mockupData[index].name!,
-                                value: mockupData[index].value!.toString(),
-                              )),
-                    )
-                  ],
-                ),
-              ),
-            ]),
-          ),
-        ]));
+              ]),
+            ),
+          ]),
+        ));
   }
 
   _getMonthsList() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: 50,
-        child: ListView.builder(
-          controller: _controller,
-          shrinkWrap: true,
-          itemBuilder: (_, index) {
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  selectedMonth = index;
-                });
-              },
-              child: Container(
-                width: 80,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: selectedMonth == index
-                      ? Theme.of(context).colorScheme.primary
-                      : Theme.of(context).colorScheme.secondary,
-                ),
-                child: Center(
-                  child: Text(
-                    months[index],
-                    style: TextStyle(
-                      color: selectedMonth == index
-                          ? Theme.of(context).colorScheme.onPrimary
-                          : Theme.of(context).colorScheme.onSecondary,
-                    ),
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: ListView.builder(
+        controller: _controller,
+        shrinkWrap: true,
+        itemBuilder: (_, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedMonth = index;
+              });
+            },
+            child: Container(
+              width: 80,
+              height: 50,
+              decoration: BoxDecoration(
+                color: selectedMonth == index
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).colorScheme.secondary,
+              ),
+              child: Center(
+                child: Text(
+                  months[index],
+                  style: TextStyle(
+                    color: selectedMonth == index
+                        ? Theme.of(context).colorScheme.onPrimary
+                        : Theme.of(context).colorScheme.onSecondary,
                   ),
                 ),
               ),
-            );
-          },
-          itemCount: 12,
-          scrollDirection: Axis.horizontal,
-        ),
+            ),
+          );
+        },
+        itemCount: 12,
+        scrollDirection: Axis.horizontal,
       ),
     );
   }
