e37559d229fb2dd539764cc5038b2199a17e3670
Gabriel Schorsch Amadeu <gabrielschorschgsa@gmail.com>
2023-09-25 11:39:46 -0300

    changed default colors

--- lib/repository/categories_repository.dart
+++ lib/repository/categories_repository.dart
@@ -3,19 +3,22 @@ import 'package:app/repository/categories_interface.dart';
 
 class CategoriesRepository implements ICategoriesRepository {
   static List<Category> mockup = [
-     Category(
+    Category(
       id: "1",
-      color: "#FF0000",
+      //pastel magenta
+      color: "#FFB6C1",
       name: "Alimentação",
     ),
     Category(
       id: "2",
-      color: "#00FF00",
+      //pastel green
+      color: "#98FB98",
       name: "Transporte",
     ),
     Category(
       id: "3",
-      color: "#0000FF",
+      //pastel blue
+      color: "#ADD8E6",
       name: "Lazer",
     ),
   ];
