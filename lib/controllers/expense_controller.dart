792416667852816194136c4195bda113e2a45fca
Gabriel Schorsch Amadeu <gabrielschorschgsa@gmail.com>
2023-09-25 11:22:31 -0300

    fix: provider not updating

--- lib/controllers/expense_controller.dart
+++ lib/controllers/expense_controller.dart
@@ -1,3 +1,4 @@
+import 'package:app/domains/category.dart';
 import 'package:app/domains/expense.dart';
 import 'package:app/repository/expense_repository.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,22 @@ class ExpenseController extends ChangeNotifier {
 
   Future<List<Expense>> getExpenses() async {
     return await repository.getExpenses();
+  }
+
+  Future<List<Expense>> getFilteredExpenses(
+      {int? month, Category? category}) async {
+    List<Expense> fullList = await repository.getExpenses();
+
+    if (category != null) {
+      fullList =
+          fullList.where((element) => element.category == category).toList();
+    }
+    if (month != null) {
+      fullList =
+          fullList.where((element) => element.date!.month == month).toList();
+    }
+    notifyListeners();
+    return fullList;
   }
 
   Future<void> addExpense(Expense expense) async {
--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,8 +1,6 @@
 import 'package:app/components/expense_card.dart';
-import 'package:app/controllers/category_controller.dart';
 import 'package:app/controllers/expense_controller.dart';
 import 'package:app/domains/category.dart';
-import 'package:app/domains/expense.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -33,23 +31,14 @@ class _DashboardState extends State<Dashboard> {
     return months[DateTime.now().month - 1];
   }
 
-  int selectedMonth = DateTime.now().month - 1;
+  late int selectedMonth;
   final ScrollController _controller = ScrollController();
   Category? filter;
-  List<Expense> expenses = [];
-  List<Expense> fullList = [];
 
   @override
   void initState() {
-    (Provider.of<ExpenseController>(context, listen: false).getExpenses())
-        .then((value) {
-      fullList = value.toList();
-    });
-
+    selectedMonth = DateTime.now().month - 1;
     super.initState();
-    expenses = fullList
-        .where((element) => element.date!.month == selectedMonth + 1)
-        .toList();
   }
 
   @override
@@ -57,7 +46,7 @@ class _DashboardState extends State<Dashboard> {
     _controller.animateTo(selectedMonth * 80.0 - 160,
         duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
 
-    buildHistory(BuildContext context) {
+    buildHistory(BuildContext context, ExpenseController expenseController) {
       return Container(
         width: MediaQuery.of(context).size.width * .8,
         decoration: BoxDecoration(
@@ -85,11 +74,6 @@ class _DashboardState extends State<Dashboard> {
                           onPressed: () {
                             setState(() {
                               filter = null;
-
-                              expenses = fullList
-                                  .where((element) =>
-                                      element.date!.month == selectedMonth + 1)
-                                  .toList();
                             });
                           },
                           icon: const Icon(Icons.clear),
@@ -103,20 +87,22 @@ class _DashboardState extends State<Dashboard> {
                 left: 15,
                 top: 20,
               ),
-              child: Consumer<ExpenseController>(
-                builder: (context, expenseController, child) => FutureBuilder(
-                    future: expenseController.getExpenses(),
-                    builder: (context, expenseSnapshot) {
-                      return ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: expenses.length,
-                          itemBuilder: (_, index) => ExpenseCard(
-                                color: Color(int.parse(
-                                    "0xFF${expenses[index].category!.color!.replaceAll('#', '')}")),
-                                name: expenses[index].name!,
-                                value: expenses[index].value!.toString(),
-                              ));
-                    }),
+              child: FutureBuilder(
+                future: expenseController.getFilteredExpenses(
+                    month: selectedMonth + 1, category: filter),
+                builder: (context, expenseSnapshot) {
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: (expenseSnapshot.data ?? []).length,
+                      itemBuilder: (_, index) => ExpenseCard(
+                            color: Color(int.parse(
+                                "0xFF${(expenseSnapshot.data ?? [])[index].category!.color!.replaceAll('#', '')}")),
+                            name: (expenseSnapshot.data ?? [])[index].name!,
+                            value: (expenseSnapshot.data ?? [])[index]
+                                .value!
+                                .toString(),
+                          ));
+                },
               ),
             )
           ],
@@ -124,7 +110,7 @@ class _DashboardState extends State<Dashboard> {
       );
     }
 
-    buildChart(BuildContext context, List<Category> categories) {
+    buildChart(BuildContext context, ExpenseController expenseController) {
       return SizedBox(
         width: MediaQuery.of(context).size.width * .8,
         child: Column(
@@ -134,47 +120,40 @@ class _DashboardState extends State<Dashboard> {
               child: SizedBox(
                 height: 200,
                 width: 200,
-                child: Consumer<ExpenseController>(
-                  builder: (context, expenseController, child) => FutureBuilder(
-                    future: expenseController.getExpenses(),
-                    builder: (context, expenseSnapshot) {
-                      return PieChart(
-                        PieChartData(
-                          sections: expenses
-                              .map(
-                                (expense) => PieChartSectionData(
-                                  color: Color(int.parse(
-                                      "0xFF${expense.category!.color!.replaceAll('#', '')}")),
-                                  value: expense.value!.toDouble(),
-                                  showTitle: false,
-                                  // title: expense.category!.name!,
-                                  radius: 50,
-                                ),
-                              )
-                              .toList(),
-                          pieTouchData: PieTouchData(
-                            enabled: true,
-                            touchCallback: (p0, p1) {
-                              var originalList = expenseSnapshot.data!;
-                              var selectedElement = originalList[
-                                  p1!.touchedSection!.touchedSectionIndex];
-                              setState(() {
-                                filter = selectedElement.category;
-
-                                expenses = expenses
-                                    .where(
-                                      (element) =>
-                                          element.category!.id ==
-                                          selectedElement.category!.id,
-                                    )
-                                    .toList();
-                              });
-                            },
-                          ),
-                        ),
-                      );
-                    },
+                child: FutureBuilder(
+                  future: expenseController.getFilteredExpenses(
+                    category: filter,
+                    month: selectedMonth + 1,
                   ),
+                  builder: (context, expenseSnapshot) {
+                    return PieChart(
+                      PieChartData(
+                        sections: (expenseSnapshot.data ?? [])
+                            .map(
+                              (expense) => PieChartSectionData(
+                                color: Color(int.parse(
+                                    "0xFF${expense.category!.color!.replaceAll('#', '')}")),
+                                value: expense.value!.toDouble(),
+                                showTitle: false,
+                                // title: expense.category!.name!,
+                                radius: 50,
+                              ),
+                            )
+                            .toList(),
+                        pieTouchData: PieTouchData(
+                          enabled: true,
+                          touchCallback: (p0, p1) {
+                            var originalList = expenseSnapshot.data!;
+                            var selectedElement = originalList[
+                                p1!.touchedSection!.touchedSectionIndex];
+                            setState(() {
+                              filter = selectedElement.category;
+                            });
+                          },
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
             ),
@@ -184,29 +163,29 @@ class _DashboardState extends State<Dashboard> {
     }
 
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           toolbarHeight: 75,
           leading: _getMonthsList(),
           leadingWidth: MediaQuery.of(context).size.width,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomScrollView(slivers: [
-            SliverList(
-              delegate: SliverChildListDelegate([
-                Consumer<CategoryController>(
-                  builder: (context, categoryController, child) =>
-                      FutureBuilder(
-                    future: categoryController.getCategories(),
-                    builder: (context, snapshot) {
-                      return buildChart(context, snapshot.data ?? []);
-                    },
+        body: Consumer<ExpenseController>(
+          builder: (BuildContext context, ExpenseController expenseController,
+                  Widget? child) =>
+              Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CustomScrollView(slivers: [
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  buildChart(context, expenseController),
+                  buildHistory(
+                    context,
+                    expenseController,
                   ),
-                ),
-                buildHistory(context),
-              ]),
-            ),
-          ]),
+                ]),
+              ),
+            ]),
+          ),
         ));
   }
 
@@ -222,11 +201,6 @@ class _DashboardState extends State<Dashboard> {
             onTap: () {
               setState(() {
                 selectedMonth = index;
-
-                expenses = fullList
-                    .where(
-                        (element) => element.date!.month == selectedMonth + 1)
-                    .toList();
               });
             },
             child: Container(
