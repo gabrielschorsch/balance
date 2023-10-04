c3586d9746c1fb7a5708100fa00ec6d4fac03523
Gabriel Schorsch <gabrielschorschgsa@gmail.com>
2023-10-04 17:38:49 -0300

    added configuration for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ import 'package:app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'testing',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
